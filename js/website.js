a5ec6a2ca5166b59347098f419855670fab273be
guillaumesoullard <guillaume.soullard@gmail.com>
2018-06-11 22:50:35 +0200

    test scroll single page

--- js/website.js
+++ js/website.js
@@ -1,26 +1,42 @@
-const nbScrollPage = 3
+const demoStepLength = 5
+const stepScrollLength = 3
 
 $(document).ready(function () {
-	$(".region").hide()
 
-	/*$(".region.lower.third").show()
-
-	var height = $(window).height()
-	var heightComplimeent = height*0.66
-	$(".region.lower").css("top",heightComplimeent+"px")
-
-	$(".region.top").show()*/
-
+	let demoStep = 0
 	let stepScroll = 0
 
+	initPresentation()
+
 	$(window).on("click", function () {
-		(stepScroll >= nbScrollPage) ? stepScroll = 0 : stepScroll++
-		displayNextStep(stepScroll)
+		displayNextStepDemo(demoStep)
 	});
 });
 
+function initPresentation() {
+	$(".region").hide()
+	$(".region.lower.third").show()
+}
+
+function displayNextStepDemo(demoStep) {
+	(demoStep >= demoStepLength) ? demoStep = 0 : demoStep++
+	displayElementDemoStep(demoStep)
+	/*(stepScroll >= stepScrollLength) ? stepScroll = 0 : stepScroll++
+	scrollNextSection(stepScroll)*/
+}
+
+function displayElementDemoStep(demoStep) {
+	switch(demoStep) {
+	case 1:
+		$(".region.top").show()
+		break;
+	case 2:
+		break;
+	}
+}
+
 // single page scroll manager
 // scroll height page
-function displayNextStep(stepScroll) {
+function scrollNextSection(stepScroll) {
 	$("html, body").animate( { scrollTop: $(window).height()*stepScroll }, 1000 )
 };
