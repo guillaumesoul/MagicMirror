a3dc64da01a11a7068942ac8190bc4d05839c957
guillaumesoullard <guillaume.soullard@gmail.com>
2018-06-11 22:28:49 +0200

    test scroll single page

--- js/website.js
+++ js/website.js
@@ -1,16 +1,26 @@
+const nbScrollPage = 3
+
 $(document).ready(function () {
-	$(".region").hide();
+	$(".region").hide()
 
-	$(".region.lower.third").show();
+	/*$(".region.lower.third").show()
 
-	var height = $(window).height();
-	console.log(height);
-	var heightComplimeent = height*0.66;
-	$(".region.lower").css("top",heightComplimeent+"px");
+	var height = $(window).height()
+	var heightComplimeent = height*0.66
+	$(".region.lower").css("top",heightComplimeent+"px")
 
-	$(".region.top").show();
+	$(".region.top").show()*/
 
-	$(window).on("scroll", function () {
-		console.log("scroll");
+	let stepScroll = 0
+
+	$(window).on("click", function () {
+		(stepScroll >= nbScrollPage) ? stepScroll = 0 : stepScroll++
+		displayNextStep(stepScroll)
 	});
 });
+
+// single page scroll manager
+// scroll height page
+function displayNextStep(stepScroll) {
+	$("html, body").animate( { scrollTop: $(window).height()*stepScroll }, 1000 )
+};
