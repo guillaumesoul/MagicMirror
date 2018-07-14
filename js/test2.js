3f240480d90108c565de8711a85f6016c4190124
guillaumesoullard <guillaume.soullard@gmail.com>
2018-07-14 15:33:52 +0200

    try to compress imgaes with webpack

---
+++ js/test2.js
@@ -0,0 +1,1 @@
+console.log("je suis dans test2");
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+//import ImageminPlugin from 'imagemin-webpack-plugin'
 
 module.exports = {
-	//entry: './js/test.js',
-	entry: './js/cssLoader.js',
+	//entry: './js/test1.js',
+	//entry: './js/cssLoader.js',
+	/*entry: {
+		cssLoader: './js/cssLoader.js'
+	},*/
+	/*entry: [
+		'./js/defaults.js',
+		'./config/config.js',
+		'./vendor/vendor.js',
+		'./modules/default/defaultmodules.js',
+		'./js/logger.js',
+		'./translations/translations.js',
+		'./js/translator.js',
+		'./js/class.js',
+		'./js/module.js',
+		'./js/loader.js',
+		'./js/socketclient.js',
+		'./js/main.js',
+		'./js/website.js',
+		'./js/cssLoader.js',
+	],*/
+	entry: [
+		'./js/website.js',
+		'./js/cssLoader.js'
+	],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'webpack.bundle.js'
@@ -17,5 +43,16 @@ module.exports = {
 			test: /\.scss$/,
 			loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
 		}]
-	}
+	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: 'img/'
+		}]),
+		new ImageminPlugin({
+			test: 'img/bigpngs2/**',
+			optipng: {
+				optimizationLevel: 5
+			}
+		})
+	]
 };
